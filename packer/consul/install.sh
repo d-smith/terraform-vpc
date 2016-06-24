f1949c46e003a3db1806dfa1d5ece7fdefde166b
d-smith <doug.smith@fmr.com>
2016-06-24 16:03:43 -0700

    Added packer image build to boot consul client

---
+++ packer/consul/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing dependencies..."
+sudo apt-get update -y
+sudo apt-get install -y unzip
+
+echo "Fetching Consul..."
+CONSUL=0.6.4
+cd /tmp
+wget https://releases.hashicorp.com/consul/${CONSUL}/consul_${CONSUL}_linux_amd64.zip -O consul.zip
+
+echo "Installing Consul..."
+unzip consul.zip >/dev/null
+chmod +x consul
+sudo mv consul /usr/local/bin/consul
+sudo mkdir -p /opt/consul/data
+
+cat >/tmp/consul_flags << EOF
+CONSUL_FLAGS="-atlas ${ATLAS_ID}/integrations -atlas-join -atlas-token ${ATLAS_TOKEN} -data-dir=/opt/consul/data"
+EOF
+
+echo "cat the flags file"
+cat /tmp/consul_flags
+
+echo "Installing Upstart service..."
+sudo mkdir -p /etc/consul.d
+sudo mkdir -p /etc/service
+sudo chown root:root /tmp/upstart.conf 
+sudo mv /tmp/upstart.conf /etc/init/consul.conf
+sudo chmod 0644 /etc/init/consul.conf
+sudo mv /tmp/consul_flags /etc/service/consul
+sudo chmod 0644 /etc/service/consul
